72bcdef4d6cf2c3c54e658f26e4b21bfbe95295d
Adam "Ir7_o" Watson <adamwatson0@outlook.com>
2014-12-02 15:23:45 +1030

    Cleaned merging errors
    Removed commit merging errors

--- src/Definitions.h
+++ src/Definitions.h
@@ -3,7 +3,6 @@
 #define COUTL writeDebugStreamLine
 
 // :: Motors ::
-<<<<<<< HEAD
 #define Motor_Drive_Left motorF
 #define Motor_Drive_Right motorI
 
@@ -18,17 +17,6 @@
 #define Servo_Arm_Right servo6
 
 #define Servo_GoalKeeper servo3
-=======
-#define Motor_Drive_Left motorD
-#define Motor_Drive_Right motorE
-
-#define Motor_Harvester motorA
-
-// :: Servos ::
-
-
-
->>>>>>> e8f6f8f209d5c04c66dcf200a549329bb3d6574f
 
 /*
 * Returns the current time value of a timer
